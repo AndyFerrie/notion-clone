ebcf0427d4058266c9f8b50e4f0255ba92747810
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-17 08:43:47 +0100

    Fixes search command

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -14,7 +14,6 @@ import {
     CommandItem,
     CommandList,
 } from "@/components/ui/command";
-
 import { useSearch } from "@/hooks/use-search";
 import { api } from "@/convex/_generated/api";
 
@@ -55,30 +54,31 @@ export const SearchCommand = () => {
 
     return (
         <CommandDialog open={isOpen} onOpenChange={onClose}>
-            <CommandInput placeholder={`Search ${user?.fullName}'s Jotion`}>
-                <CommandList>
-                    <CommandEmpty>No results found</CommandEmpty>
-                    <CommandGroup heading="Documents">
-                        {documents?.map((document) => (
-                            <CommandItem
-                                key={document._id}
-                                value={`${document._id}-${document.title}`}
-                                title={document.title}
-                                onSelect={onSelect}
-                            >
-                                {document.icon ? (
-                                    <p className="mr-2 text-[18px]">
-                                        {document.icon}
-                                    </p>
-                                ) : (
-                                    <File className="mr-2 h-4 w-4" />
-                                )}
-                                <span>{document.title}</span>
-                            </CommandItem>
-                        ))}
-                    </CommandGroup>
-                </CommandList>
-            </CommandInput>
+            <CommandInput
+                placeholder={`Search ${user?.fullName}'s Jotion...`}
+            />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Documents">
+                    {documents?.map((document) => (
+                        <CommandItem
+                            key={document._id}
+                            value={`${document._id}-${document.title}`}
+                            title={document.title}
+                            onSelect={() => onSelect(document._id)}
+                        >
+                            {document.icon ? (
+                                <p className="mr-2 text-[18px]">
+                                    {document.icon}
+                                </p>
+                            ) : (
+                                <File className="mr-2 h-4 w-4" />
+                            )}
+                            <span>{document.title}</span>
+                        </CommandItem>
+                    ))}
+                </CommandGroup>
+            </CommandList>
         </CommandDialog>
     );
 };
