f574daa55f1a8e3062cfccb4a0268381e4887dd8
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2023-10-21 07:23:51 +0100

    Completes menu component and adds to navbar

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -14,7 +14,8 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import { MoreHorizontal } from "lucide-react";
+import { MoreHorizontal, Trash } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface MenuProps {
     documentId: Id<"documents">;
@@ -41,10 +42,29 @@ export const Menu = ({ documentId }: MenuProps) => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button>
-                    <MoreHorizontal />
+                <Button size="sm" variant="ghost">
+                    <MoreHorizontal className="h-4 w-4" />
                 </Button>
             </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="w-60"
+                align="end"
+                alignOffset={8}
+                forceMount
+            >
+                <DropdownMenuItem onClick={onArchive}>
+                    <Trash className="h-4 w-4 mr-2" />
+                    Delete
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <div className="text-xs text-muted-foreground p-2">
+                    Last edited by: {user?.fullName}
+                </div>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+    return <Skeleton className="h-10 w-10" />;
 };
